2e2c60f1fa0067fc09e853a583149d946626cff2
Afrah Aden <afrahaden05@gmail.com>
2023-12-16 10:55:49 +0300

    Modify alert message

--- static/js/script.js
+++ static/js/script.js
@@ -17,3 +17,25 @@ function copyText() {
     // Optionally provide feedback to the user (e.g., display a tooltip)
     alert("Text copied to clipboard!");
 }
+
+// Copy translated text
+
+function copyText() {
+    // Get the text to copy
+    var translatedText = document.getElementById("translatedText");
+
+    // Create a temporary textarea to hold the text
+    var textarea = document.createElement("textarea");
+    textarea.value = translatedText.textContent;
+    document.body.appendChild(textarea);
+
+    // Select and copy the text
+    textarea.select();
+    document.execCommand("copy");
+
+    // Remove the temporary textarea
+    document.body.removeChild(textarea);
+
+    // Show the custom modal
+    $('#copyTextModal').modal('show');
+}
